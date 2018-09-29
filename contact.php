03f735a2e20e947fa37ee631ed26a37de7958aa9
astie086 <astie086@gmail.com>
2018-09-28 18:44:24 -0700

    links for pages setup and color updates

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php
+
+    // set up variables //
+
+    $name = $_POST ['name'];
+    $email = $_POST ['email'];
+    $message = $_POST ['message'];
+
+    // what to do when you get the items and what is going to be sent in the message//
+    
+    $email_from = 'Site Contact';
+    $email_subject = 'New Message!';
+    $email_body = "Name: $name.\n".
+                  "Email: $email.\n".
+                  "Message: $message.\n";
+    
+    // where is it going? //
+
+    $to = "astie086@gmail.com";
+    $header = "From: $email_from \r\n";
+    $header .= "Reply-To: $email \r\n";
+
+    //what should mail do?//
+
+    mail($to,$email_subject,$email_body,$headers)
+
+    //once submitted where does that page take them?//
+
+    header("location: index.html");
+    
+?>
