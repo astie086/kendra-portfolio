fa76f22b2ef324c14d51e2e6455e587f0cb4cefc
Alvin G <abgodoy@cpp.edu>
2018-10-03 15:52:50 -0700

    Added semi-colon to terminate the line

--- contact.php
+++ contact.php
@@ -7,13 +7,13 @@
     $message = $_POST ['message'];
 
     // what to do when you get the items and what is going to be sent in the message//
-    
+
     $email_from = 'Site Contact';
     $email_subject = 'New Message!';
     $email_body = "Name: $name.\n".
                   "Email: $email.\n".
                   "Message: $message.\n";
-    
+
     // where is it going? //
 
     $to = "astie086@gmail.com";
@@ -22,10 +22,10 @@
 
     //what should mail do?//
 
-    mail($to,$email_subject,$email_body,$headers)
+    mail($to,$email_subject,$email_body,$headers);
 
     //once submitted where does that page take them?//
 
     header("location: index.html");
-    
+
 ?>
